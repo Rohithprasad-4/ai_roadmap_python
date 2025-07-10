bd4c5f78779bba93707b1a144d891a93376cdbf0
Rohithprasad-4 <wsrohithprasad@gmail.com>
2025-07-10 08:42:38 +0530

    day1_basics/
    Added Day 1 programs (Python basics) into day1_basics folder

---
+++ factorial.py
@@ -0,0 +1,8 @@
+a = int(input("enter a number"))
+result = 1
+i = 1
+
+while i <= a:
+    result = i*result
+    i = i + 1
+print("factrial of ",result)
---
+++ reverse_list.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,5]
+a.reverse()
+print(a)
+
+#without any fun
+b = [1,2,3,4,5]
+c = b[::-1]
+print(c)
